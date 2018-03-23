6bdbdfe154154e0d8c14f381ad1a0cf13387a167
Tobias Hilbrich <thilbrich@fakt.com>
2018-03-23 15:37:00 +0100

    Newest Changes

--- src/main/java/ParcelSizeServer.java
+++ src/main/java/ParcelSizeServer.java
@@ -1,6 +1,6 @@
-public class main {
+public class ParcelSizeServer {
     public static void main(String[] args){
         System.out.println("asdf");
-        System.out.println("dafuq");
+
     }
 }
